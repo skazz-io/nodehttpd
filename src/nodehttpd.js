d04850d9a24b3f5c650db9f210e1d75e8d9010a3
SkAzZ <github@skazz.io>
2021-01-20 21:55:30 +0000

    Preparing config management
     - Preparing config management by turning it into a config object;
     - Added singlethreaded mode if NODE_HTTP_THREADS is 0 (not 1 because it maintains behaviour of being a worker process);

--- src/nodehttpd.js
+++ src/nodehttpd.js
@@ -4,63 +4,79 @@ const { promises: fs, createReadStream } = require('fs');
 const cluster = require('cluster');
 const { cpus } = require('os');
 
-const wwwroot = process.env.NODE_HTTP_ROOT ? process.env.NODE_HTTP_ROOT : './';
-const port = process.env.NODE_HTTP_PORT ? Number(process.env.NODE_HTTP_PORT) : 8080;
-const threads = process.env.NODE_HTTP_THREADS ? Number(process.env.NODE_HTTP_THREADS) : cpus().length;
-
-const defaultFile = 'index.html';
-const mime = {
-    '.html': 'text/html',
-    '.css': 'text/css',
-    '.js': 'text/javascript',
-    '.gif': 'image/gif',
-    '.jpg': 'image/jpeg',
-    '.png': 'image/png',
-    '.ico': 'image/x-icon',
-    '.svg': 'image/svg+xml',
-    '.eot': 'appliaction/vnd.ms-fontobject',
-    '.ttf': 'appliaction/font-sfnt',
-    '.json': 'appliaction/json',
-    '.webmanifest': 'application/manifest+json'
-};
-const defaultHeaders = {
-    // 'Strict-Transport-Security': ['max-age=31536000', 'includeSubDomains'],
-    'X-Frame-Options': 'deny',
-    'X-Content-Type-Options': 'nosniff',
-    // 'Content-Security-Policy': 'script-src self',
-    'X-Permitted-Cross-Domain-Policies': 'none',
-    'Referrer-Policy': 'no-referrer',
-    // 'Feature-Policy': ["vibrate 'none'", "geolocation 'none'"],
-    // 'Access-Control-Allow-Origin': ''
-};
-const cors = false;
-const corsHeaders = {
-    'Access-Control-Allow-Origin': `https://127.0.0.1:${port}`,
-    'Access-Control-Allow-Methods': 'POST, GET, OPTIONS',
-    'Access-Control-Allow-Headers': 'X-PINGOTHER, Content-Type',
-    'Access-Control-Max-Age': 86400
+var config = {
+    wwwroot: './',
+    port: 8080,
+    threads: cpus().length,
+    defaultFile: 'index.html',
+    allowHidden: false,
+    mime: {
+        '.html': 'text/html',
+        '.css': 'text/css',
+        '.webmanifest': 'application/manifest+json',
+        '.js': 'text/javascript',
+        '.jpg': 'image/jpeg',
+        '.png': 'image/png',
+        '.ico': 'image/x-icon',
+        '.svg': 'image/svg+xml',
+        '.eot': 'appliaction/vnd.ms-fontobject',
+        '.ttf': 'appliaction/font-sfnt',
+        '.json': 'appliaction/json'
+    },
+    defaultHeaders: {
+        // 'Strict-Transport-Security': ['max-age=31536000', 'includeSubDomains'],
+        'X-Frame-Options': 'deny',
+        'X-Content-Type-Options': 'nosniff',
+        // 'Content-Security-Policy': 'script-src self',
+        'X-Permitted-Cross-Domain-Policies': 'none',
+        'Referrer-Policy': 'no-referrer',
+        // 'Feature-Policy': ["vibrate 'none'", "geolocation 'none'"],
+        // 'Access-Control-Allow-Origin': ''
+    },
+    cors: true,
+    corsHeaders: {
+        'Access-Control-Allow-Origin': 'https://127.0.0.1:8080',
+        'Access-Control-Allow-Methods': 'POST, GET, OPTIONS',
+        'Access-Control-Allow-Headers': 'X-PINGOTHER, Content-Type',
+        'Access-Control-Max-Age': 86400
+    }
 }
 
-const chroot = path.resolve(wwwroot);
-const defaultExt = path.extname(defaultFile);
+config.wwwroot = process.env.NODE_HTTP_ROOT ? process.env.NODE_HTTP_ROOT : config.wwwroot;
+config.port = process.env.NODE_HTTP_PORT ? Number(process.env.NODE_HTTP_PORT) : config.port;
+config.threads = process.env.NODE_HTTP_THREADS ? Number(process.env.NODE_HTTP_THREADS) : config.threads;
 
-if (cluster.isMaster) {
-    console.log(`Server starting on port: ${port}`)
-    
-    cluster.on('fork', function(worker) {
-        console.log('worker started', worker.process.pid)
-    });
-
-    cluster.on('exit', function(worker, code, signal) {
-        console.log('worker died', worker.process.pid, code, signal);
-        cluster.fork();
-    });
-
-    for (var i = 0; i < threads; i++) {
-        cluster.fork({ NODE_CHILD: i });
+if (config.port != 8080 && config.corsHeaders && config.corsHeaders['Access-Control-Allow-Origin']) {
+    if (config.corsHeaders['Access-Control-Allow-Origin'].endsWith('8080')) {
+        config.corsHeaders['Access-Control-Allow-Origin'] = `'https://127.0.0.1:${config.port}'`
     }
+}
 
-    return;
+const chroot = path.resolve(config.wwwroot);
+const defaultExt = path.extname(config.defaultFile);
+
+if (config.threads > 0) {
+    if (cluster.isMaster) {
+        console.log(`Server starting on port ${config.port} with ${config.threads} workers.`)
+
+        cluster.on('fork', function(worker) {
+            console.log('worker started', worker.process.pid)
+        });
+
+        cluster.on('exit', function(worker, code, signal) {
+            console.log('worker died', worker.process.pid, code, signal);
+            cluster.fork();
+        });
+
+        for (var i = 0; i < config.threads; i++) {
+            cluster.fork({ NODE_CHILD: i });
+        }
+
+        return;
+    }
+} else {
+    console.log(`Server starting on port ${config.port} singlethreaded.`)
+    sendLog = console.log;
 }
 
 http.createServer(async function (req, res) {
@@ -69,13 +85,13 @@ http.createServer(async function (req, res) {
     var error = null;
 
     try {
-        for (var key in defaultHeaders) {
-            res.setHeader(key, defaultHeaders[key]);
+        for (var key in config.defaultHeaders) {
+            res.setHeader(key, config.defaultHeaders[key]);
         }
 
-        if (cors && req.method == 'OPTIONS') {
-            for (var key in corsHeaders) {
-                res.setHeader(key, corsHeaders[key]);
+        if (config.cors && req.method == 'OPTIONS') {
+            for (var key in config.corsHeaders) {
+                res.setHeader(key, config.corsHeaders[key]);
             }
             res.writeHead(204);
             return;
@@ -93,7 +109,7 @@ http.createServer(async function (req, res) {
 
         url = new URL(req.url, `http://${host}`);
 
-        let localPath = path.resolve(path.join(wwwroot, url.pathname));
+        let localPath = path.resolve(path.join(config.wwwroot, url.pathname));
 
         if (!localPath.startsWith(chroot)) {
             throw 404;
@@ -102,11 +118,11 @@ http.createServer(async function (req, res) {
         let localExt = path.extname(localPath);
 
         if (!localExt) {
-            localPath = path.join(localPath, defaultFile);
+            localPath = path.join(localPath, config.defaultFile);
             localExt = defaultExt;
         }
 
-        let localMime = mime[localExt];
+        let localMime = config.mime[localExt];
 
         if (!localMime) {
             throw 404;
@@ -170,4 +186,4 @@ http.createServer(async function (req, res) {
             error: error
         });
     }
-}).listen(port);
+}).listen(config.port);
